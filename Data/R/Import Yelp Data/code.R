efa6d5f7f8405cc02e0690dfef064bc363bfdc16
rl627 <rl627@drexel.edu>
2021-11-30 09:57:14 -0500

    Cleaned yelp data

--- Data/R/Import Yelp Data/code.R
+++ Data/R/Import Yelp Data/code.R
@@ -5,14 +5,36 @@
   library(rjson)
   library(data.table)
   library(rstudioapi)
+  library(jsonlite)
+  library(stringr)
   
   ## Set working directory to file location
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 }
 
-{ # 1. Read in Yelp  ---- 
+{ # 1. Yelp Data  ---- 
   
-  ## Try with JSON Light
-  dfa = fread("../../Raw Data/yelp_business.csv")
+  ## Stream in NDJSON (Newline deliminted JSON)
+  yelp_raw  = stream_in(file("../../Raw Data/yelp_academic_dataset_business.json")) %>% 
+    as_tibble() %>% 
+    select(id = business_id, name, city,lat= latitude, lng = longitude, 
+           stars, review_count, 
+           categories)
+
+  ## Subset data
+  df_subset = yelp_raw %>% filter(city%in%c("Austin","Portland","Boston"))
+    
   
+  ## Clean attributes 
+  cleaned__tidy_yelp = df_subset %>% 
+    mutate(grocery = str_detect(categories,"Grocery"),
+           health_market = str_detect(categories,'Health Markets'),
+           restaurants = str_detect(categories,"Restaurants"),
+           fast_food =str_detect(categories,"Fast Food")) 
+}
+ 
+
+{ # 2. Save-----
+  save(cleaned__tidy_yelp, file = "Clean/cleaned__tidy_yelp.rdata")
+  fwrite(cleaned__tidy_yelp, file = "Clean/cleaned__tidy_yelp.csv")
 }
