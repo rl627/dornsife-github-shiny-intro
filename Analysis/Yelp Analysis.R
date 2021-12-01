39134f4b27a2c96f9d6a3c914d327c24907e4a36
rl627 <rl627@drexel.edu>
2021-11-30 19:40:10 -0500

    minimized Analysis.R

--- Analysis/Yelp Analysis.R
+++ Analysis/Yelp Analysis.R
@@ -1,35 +1,30 @@
-{ # 0. Setup -----
-  ## Dependencies
-  library(tidyr)
-  library(dplyr)
-  library(leaflet)
-  library(purrr)
-  library(shiny)
-  library(ggplot2)
-  library(plotly)
-  library(reactable)
-  library(sf)
-  library(sjPlot)
-  load("R/Data/analysis_bundle.rdata")
-  source("R/Figures/code_prep_data_for_analysis.R")
-  source("R/Figures/code_make_boxplot.R")
-  source("R/Figures/code_make_map.R")
+### 1. Dependencies
+library(tidyr)
+library(dplyr)
+library(leaflet)
+library(purrr)
+library(shiny)
+library(ggplot2)
+library(plotly)
+library(reactable)
+library(sf)
+library(sjPlot)
+load("R/Data/analysis_bundle.rdata")
+source("R/Figures/code_prep_data_for_analysis.R")
+source("R/Figures/code_make_boxplot.R")
+source("R/Figures/code_make_map.R")
 
-  ## Set working directory to file location (Not for Shiny!)
-  library(rstudioapi)
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-}
 
-### 1. Prepare Data
+### 2. Preparing 
 cityTmp  = "Austin"; exposureTmp = "% Complete College"; outcomeTmp = "healthy_grocery"
 dataTmp = prep_data_for_analysis(cityTmp,  exposureTmp,   outcomeTmp)
 
-### 2. Visualize Data
+### 3. Visualizing
 make_boxplot(dataTmp)
 
-### 3. Map Data
+### 4. Maping 
 make_map(dataTmp,shape_file_zcta )
 
-### 4. Model Data
+### 4. Modeling 
 model = glm(outcome_value~exposure_value ,family=binomial(link='logit'),data=dataTmp$dfMerge)
 tab_model(model)
