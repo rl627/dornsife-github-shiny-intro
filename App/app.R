52123a9d067442a8303d53796bcd25026db0a4e9
rl627 <rl627@drexel.edu>
2021-11-30 21:48:16 -0500

    Revert "Step 4.1 Imported dependencies + data"
    This reverts commit 2b1f0464be4f4c04d8267d7ef9b9693dbad16a77.

--- App/app.R
+++ App/app.R
@@ -1,18 +1,4 @@
-## Dependencies
-library(tidyr)
-library(dplyr)
-library(leaflet)
-library(purrr)
 library(shiny)
-library(ggplot2)
-library(plotly)
-library(reactable)
-library(sf)
-library(sjPlot)
-load("R/Data/analysis_bundle.rdata")
-source("R/Figures/code_prep_data_for_analysis.R")
-source("R/Figures/code_make_boxplot.R")
-source("R/Figures/code_make_map.R")
 
 # Define UI for application
 ui <- fluidPage(
